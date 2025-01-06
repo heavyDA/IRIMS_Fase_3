5a739a3a63510a5c8622853a7f4ba1fe9180f2ff
Maulana Zain <maulanadezain@gmail.com>
2025-01-06 16:22:12 +0700

    fix(vite): fixing fonts not copied during build

--- vite.config.js
+++ vite.config.js
@@ -7,39 +7,32 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/icons.css',
                 'resources/js/app.js',
                 'resources/js/pages/RBAC/users/index.js',
                 'resources/js/pages/risk/assessment/index.js',
                 'resources/js/pages/risk/assessment/worksheet/index.js',
                 'resources/js/pages/risk/assessment/worksheet/edit.js',
                 'resources/js/pages/risk/assessment/worksheet/table_view.js',
+                'resources/js/pages/risk/process/index.js',
             ],
             refresh: true,
         }),
     ],
-
     resolve: {
         alias: {
             '@': '/node_modules',
             'css': '/resources/css',
             'js': '/resources/js',
-            '~fonts': path.resolve(__dirname, 'resources/css/fonts')
+            '~fonts': path.resolve(__dirname, 'resources/fonts')
         },
     },
-
-    server: {
-        fs: {
-            strict: false
-        }
-    },
-
     build: {
-        assetsDir: 'assets',
+        target: 'esnext',
+        assetsInlineLimit: 0,
+        outDir: 'public/build',
+        emptyOutDir: true,
         rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-                css: 'resources/css/app.css',
-            },
             output: {
                 assetFileNames: (assetInfo) => {
                     let extType = assetInfo.name.split('.')[1];
@@ -48,14 +41,11 @@ export default defineConfig({
                     } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
                         extType = 'fonts';
                     }
-                    return `assets/${extType}/[name][extname]`;
+                    return `assets/${extType}/[name]-[hash][extname]`;
                 },
                 chunkFileNames: 'assets/js/[name]-[hash].js',
                 entryFileNames: 'assets/js/[name]-[hash].js',
             },
         },
     },
-
-    publicDir: 'public',
-    base: '/',
 });
